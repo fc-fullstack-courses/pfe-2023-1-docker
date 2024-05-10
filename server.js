af60ffe33d2899d729f2cef20ed453d3a24781af
Kyrylo <fresh.js.ftd.@gmail.com>
2024-05-10 21:15:27 +0300

    Add basic Dockerfile example

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('magic');
+});
+
 app.get('/test', (req, res) => {
-  res.send('this is our server');
+  res.send('this is our server test');
 });
 
 app.listen(3000, () => {
   console.log('server started');
 });
+
+// створення образу на основі Dockerfile
+// docker build -t basic_server:0.0.1 .
